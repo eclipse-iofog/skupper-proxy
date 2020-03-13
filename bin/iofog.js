beb943680e256ef3759e76a10fca18cd30cc24a2
Saeid Rezaei Baghbidi <saeid@edgeworx.io>
2020-03-12 20:30:32 -0700

    Handle uncaught exceptions

--- bin/iofog.js
+++ bin/iofog.js
@@ -14,35 +14,29 @@ async function fetchConfig() {
     })
   })
 
-  try {
-    const config = await configRequest()
-    const mappings = config.mappings || []
-    const networkRouter = config.networkRouter || {}
+  const config = await configRequest()
+  const mappings = config.mappings || []
+  const networkRouter = config.networkRouter || {}
 
-    log.info('*** New Mappings: ', JSON.stringify(mappings))
-    log.info('*** Current Config: ', JSON.stringify(Object.keys(currentConfig)))
-    const toDelete = Object.keys(currentConfig).filter(key => !mappings.find(mapping => mapping === key))
-    log.info('*** To Delete: ', JSON.stringify(toDelete))
-    const toAdd = mappings.filter((key) => !currentConfig[key])
-    log.info('*** To Add: ', JSON.stringify(toAdd))
+  log.info('*** New Mappings: ', JSON.stringify(mappings))
+  log.info('*** Current Config: ', JSON.stringify(Object.keys(currentConfig)))
+  const toDelete = Object.keys(currentConfig).filter(key => !mappings.find(mapping => mapping === key))
+  log.info('*** To Delete: ', JSON.stringify(toDelete))
+  const toAdd = mappings.filter((key) => !currentConfig[key])
+  log.info('*** To Add: ', JSON.stringify(toAdd))
 
-    toDelete.forEach((mapping) => {
-      if (currentConfig[mapping].stop) {
-        log.info(`*** Closing connection for ${mapping}`)
-        currentConfig[mapping].stop()
-      }
-      delete(currentConfig[mapping])
-    })
+  toDelete.forEach((mapping) => {
+    if (currentConfig[mapping].stop) {
+      log.info(`*** Closing connection for ${mapping}`)
+      currentConfig[mapping].stop()
+    }
+    delete(currentConfig[mapping])
+  })
 
-    toAdd.forEach((mapping) => {
-      log.info(`*** Creating ${mapping}`)
-      currentConfig[mapping] = proxy(mapping, 'localhost', { routerHost: networkRouter.host, routerPort: networkRouter.port, saslEnabled: false })
-    })
-  } catch (e) {
-    console.error(e)
-    process.exit(1)
-  }
-
+  toAdd.forEach((mapping) => {
+    log.info(`*** Creating ${mapping}`)
+    currentConfig[mapping] = proxy(mapping, 'localhost', { routerHost: networkRouter.host, routerPort: networkRouter.port, saslEnabled: false })
+  })
 }
 
 async function main() {
@@ -53,8 +47,13 @@ async function main() {
       await fetchConfig()
     },
     'onError': (error) => {
-      console.error('There was an error with Control WebSocket connection to ioFog: ', error)
-      process.exit(1)
+      throw new Error('There was an error with Control WebSocket connection to ioFog: ', error)
     }
   })
 }
+
+process.on('uncaughtException', (err) => {
+  console.error((new Date).toUTCString() + ' Exception:', err.message)
+  console.error(err.stack)
+  process.exit(1)
+})
